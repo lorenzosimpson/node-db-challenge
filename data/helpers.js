89ab608caf61c8a4d42760fde42dd50b734180f2
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-08 12:41:33 -0500

    adds successful post to projects

--- data/helpers.js
+++ data/helpers.js
@@ -1,5 +1,6 @@
 const db = require('./db-config')
 
+// RESOURCES
 function getResources() {
     return db
     .select('*')
@@ -10,7 +11,23 @@ function addResource(resource) {
     return db('resources').insert(resource)
 }
 
+
+
+// PROJECTS
+function getProjects() {
+    return db
+    .select('*')
+    .from('projects')
+}
+
+function addProject(project) {
+    return db('projects').insert(project)
+}
+
+
 module.exports = {
     getResources,
     addResource,
+    getProjects,
+    addProject,
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 
 const ResourceRouter = require('./routers/ResourceRouter')
+const ProjectRouter = require('./routers/ProjectRouter')
 
 const server = express()
 server.use(express.json())
 server.use('/api/resources', ResourceRouter)
+server.use('/api/projects', ProjectRouter)
 
 module.exports = server;
