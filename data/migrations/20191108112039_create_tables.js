8dcb3095b7f6bd01006c6144dd270f03558dbfa4
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-11-08 11:32:07 -0500

    creates tasks, projects, resources tables

---
+++ data/migrations/20191108112039_create_tables.js
@@ -0,0 +1,35 @@
+
+exports.up = function(knex) {
+  return knex.schema
+
+  .createTable('resources', tbl => {
+      tbl.increments()
+      tbl.string('name')
+        .unique()
+        .notNullable()
+      tbl.string('description')
+  })
+
+  .createTable('projects', tbl => {
+      tbl.increments()
+      tbl.string('name')
+        .notNullable()
+      tbl.string('description')
+      tbl.boolean('completed').defaultTo(false)
+  })
+
+  .createTable('tasks', tbl => {
+      tbl.increments()
+      tbl.string('description')
+        .notNullable()
+      tbl.string('notes')
+      tbl.boolean('completed').defaultTo(false)
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema
+  .dropTableIfExists('tasks')
+  .dropTableIfExists('projects')
+  .dropTableIfExists('resources')
+};
